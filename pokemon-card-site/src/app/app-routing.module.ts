27a1f5ef8e1bc143faeb1a188f91bd04932f61ba
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-08-21 19:21:48 -0300

    first commit

---
+++ pokemon-card-site/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'sobre', component: SobreComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
